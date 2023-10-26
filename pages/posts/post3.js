a692d4e7473a8e1ad71ccf4cf2a205c8106e5ad8
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 <lucas.crawhallstein@ontariotechu.net>
2023-10-25 21:12:58 -0400

    Finally got rid of secret hidding sneaky cryptonite link

--- pages/posts/post3.js
+++ pages/posts/post3.js
@@ -15,7 +15,7 @@ const Post = () => {
                 <P>
                     I&apos;m back after just a few days! I&apos;ve built up a
                     crypto information hub at <Link href="/">
-                    https://crypto-nite.vercel.app</Link> to 
+                    (Unfortunately Broken)</Link> to 
                     get some practice with React Redux and I&apos;ve upgraded
                     MarkUp to support Tex syntax for math equations. I mainly 
                     planned on using MarkUp as a note taking app for school so 
